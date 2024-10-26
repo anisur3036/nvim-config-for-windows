7913ded5d635fe58094a9a3a9c24bda20fde4042
Anisur Rahman <anisurlist3@gmail.com>
2024-10-26 22:56:34 +0600

    nvim config for windows

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,9 @@
+return {
+	"nvim-tree/nvim-tree.lua", version = "*", lazy = false,
+	requires = {"nvim-tree/nvim-web-devicons",},
+	config = function()
+		require("nvim-tree").setup {}
+	end,
+
+	vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<cr>", {silent=true, noremap=true})
+}
